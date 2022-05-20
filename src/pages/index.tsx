7e955d2adf851f832a56391904fb185f92c27c42
ysuzuki <ysuzuki@bysystems.jp>
2022-05-20 12:01:48 +0900

    Add src link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 export default function Home() {
   const [val, setVal] = useState<number>(0);
   return (
-    <input type="text" value={val} onChange={evt => setVal(Number(evt.target.value))} />
+    <>
+      <a href="https://github.com/ysuzuki-bysystems/suzuki-examples/blob/main/src/pages/index.tsx">src</a>
+      <input type="text" value={val} onChange={evt => setVal(Number(evt.target.value))} />
+    </>
   );
 }
